fa6ebe1161465f013a6ff9e06db5601c50f08868
runner <runner@Mac-1672148343460.local>
2022-12-27 15:06:10 +0000

    Update Artefacts 0.0.2

---
+++ Namogoo.xcframework/Namogoo.xcframework/ios-arm64/Namogoo.framework/Headers/Namogoo.h
@@ -0,0 +1,19 @@
+//
+//  Namogoo.h
+//  Namogoo
+//
+//  Created by Sergiy Kostrykin on 03/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+#import "NMGHardwareInfo.h"
+
+//! Project version number for Namogoo.
+FOUNDATION_EXPORT double NamogooVersionNumber;
+
+//! Project version string for Namogoo.
+FOUNDATION_EXPORT const unsigned char NamogooVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Namogoo/PublicHeader.h>
+
+
